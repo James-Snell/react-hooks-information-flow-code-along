ba2c33fa3ea25afa1c31fccec62adbd8264c0626
James <jmsn311@gmail.com>
2024-07-07 16:34:31 -0600

    follow along

--- src/Parent.js
+++ src/Parent.js
@@ -3,15 +3,38 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  // const randomColor = getRandomColor();
+  // const [color, setColor] = useState(randomColor); // initial value for color state
+  //STORE THE COLOR OF CHILD IN STATE OF THE PARENT COMPONENT. THEN LET PARENT HANDLE PASSING OF DATA TO CHILD COMP
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
 
+  // function handleChangeColor() {
+  //   const newRandomColor = getRandomColor();
+  //   setColor(newRandomColor); // update color state to a new value
+  // }
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
+
+  // return (
+  //   <div className="parent" style={{ backgroundColor: color }}>
+  //     <Child onChangeColor={handleChangeColor} />
+  //     <Child onChangeColor={handleChangeColor} />
+  //   </div>
+  // );
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
 
 export default Parent;
+
+
